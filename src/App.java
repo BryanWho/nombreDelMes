bcc8098c740321320886d73d2bada090e4b45cc2
Bryan Who <brayanromeroariza@gmail.com>
2022-02-06 11:23:17 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int month = 0;
+        Scanner sc = new Scanner(System.in);
+        month = sc.nextInt();
+        if(month == 1){
+            System.out.println("output=Enero");
+        }
+        if(month == 2){
+            System.out.println("output=Febrero");
+        }
+        if(month == 3){
+            System.out.println("output=Marzo");
+        }
+        if(month == 4){
+            System.out.println("output=Abril");
+        }
+        if(month == 5){
+            System.out.println("output=Mayo");
+        }
+        if(month == 6){
+            System.out.println("output=Junio");
+        }
+        if(month == 7){
+            System.out.println("output=Julio");
+        }
+        if(month == 8){
+            System.out.println("output=Agosto");
+        }
+        if(month == 9){
+            System.out.println("output=Septiembre");
+        }
+        if(month == 10){
+            System.out.println("output=Octubre");
+        }
+        if(month == 11){
+            System.out.println("output=Noviembre");
+        }
+        if(month == 12){
+            System.out.println("output=Diciembre");
+        }
+    }
+}
