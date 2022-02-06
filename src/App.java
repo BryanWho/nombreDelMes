c09f40b83d2298e5fd301dba7a20e2dced1dd0cc
Bryan Who <brayanromeroariza@gmail.com>
2022-02-06 11:47:49 -0500

    2.0

--- src/App.java
+++ src/App.java
@@ -1,44 +1,10 @@
 import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        int month = 0;
+        int i = 0;
+        List<String> month = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "10Noviembre", "Diciembre");
         Scanner sc = new Scanner(System.in);
-        month = sc.nextInt();
-        if(month == 1){
-            System.out.println("output=Enero");
-        }
-        if(month == 2){
-            System.out.println("output=Febrero");
-        }
-        if(month == 3){
-            System.out.println("output=Marzo");
-        }
-        if(month == 4){
-            System.out.println("output=Abril");
-        }
-        if(month == 5){
-            System.out.println("output=Mayo");
-        }
-        if(month == 6){
-            System.out.println("output=Junio");
-        }
-        if(month == 7){
-            System.out.println("output=Julio");
-        }
-        if(month == 8){
-            System.out.println("output=Agosto");
-        }
-        if(month == 9){
-            System.out.println("output=Septiembre");
-        }
-        if(month == 10){
-            System.out.println("output=Octubre");
-        }
-        if(month == 11){
-            System.out.println("output=Noviembre");
-        }
-        if(month == 12){
-            System.out.println("output=Diciembre");
-        }
+        i = sc.nextInt();
+        System.out.println("output="+month.get(i-1));
     }
 }
