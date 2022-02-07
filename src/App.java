1d0326992cf1300a025db5af12cd714cfae8b389
Bryan Who <brayanromeroariza@gmail.com>
2022-02-06 20:29:11 -0500

    2.0

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         int i = 0;
-        List<String> month = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "10Noviembre", "Diciembre");
+        List<String> month = Arrays.asList("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
         Scanner sc = new Scanner(System.in);
         i = sc.nextInt();
         System.out.println("output="+month.get(i-1));
